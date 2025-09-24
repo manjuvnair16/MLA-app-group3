5257a1efec2bb84118c825ec50fc17be38df6733
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2025-09-24 09:21:27 +0100

    fix failing test

--- frontend/src/components/login.test.js
+++ frontend/src/components/login.test.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import axios from 'axios';
 import Login from './login';
+import { MemoryRouter } from 'react-router-dom';
 
 jest.mock('axios');
 
 it('renders login form', () => {
-    render(<Login onLogin={jest.fn()} />);
+    render(
+        <MemoryRouter>
+            <Login onLogin={jest.fn()} />
+        </MemoryRouter>
+    );
     expect(screen.getByLabelText(/Username/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Password/i)).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /Login/i })).toBeInTheDocument();
